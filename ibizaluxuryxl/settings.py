508624d6ed4147611df0154a1c4b26a1c865c9d9
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-03-24 23:42:07 +0530

    added static dirs and added public ip in allowed hosts

--- ibizaluxuryxl/settings.py
+++ ibizaluxuryxl/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from decouple import config
 from celery.schedules import crontab
-
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b^=cq44s06&u%n5&+gk*j_ph_48a&hv_$umtea@&vsx2@#+=6e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '16.171.140.188']
 
 
 # Application definition
@@ -143,6 +143,10 @@ CELERY_BEAT_SCHEDULE = {
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
