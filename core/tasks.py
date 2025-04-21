6482dcb715a41d607a004c093c5ed679c5973b79
Eaby Thomas C <eabythomascu@gmail.com>
2025-04-22 01:38:12 +0530

    refresh token task changes

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,8 @@
 from celery import shared_task
-from .services import OAuthServices
+# from .services import OAuthServices
+from management.commands.refresh_all_token import Command
 
 @shared_task
 def refresh_token_task():
-    OAuthServices.refresh_access_token()
+    Command.handle()
     print("Token refresh task executed.")
